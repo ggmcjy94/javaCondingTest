6902cb237e32b78bdf02a215ab841e7638569505
ggmcjy <ggmcjy94@naver.com>
2022-03-18 17:10:03 +0900

    java commit

--- src/main/java/test04/Exam43.java
+++ src/main/java/test04/Exam43.java
@@ -1,13 +1,27 @@
 package test04;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Exam43 {
 
-    public int[] solution(int n, int k, int[] arr) {
-        int[] answer = new int[]{};
+    public ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
+        for (int i = 0; i < k-1; i++) { // lt 를 3개 담고
+            HM.put(arr[i], HM.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int lt = 0;
+        for (int rt=k-1; rt < n; rt++) {
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0) + 1); //rt 한개담고
+            //슬라이딩 시작
+            answer.add(HM.size()); // 3 4 4 3
+            HM.put(arr[lt], HM.get(arr[lt])-1);
+            if (HM.get(arr[lt]) == 0) {
+                HM.remove(arr[lt]);
+            }
+            lt++;
+        }
         return answer;
     }
 
