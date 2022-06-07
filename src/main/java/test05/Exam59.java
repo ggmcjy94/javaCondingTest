bd12a8bc4a09ae87aadd4c2d02f0e6de2bb9f764
“ggmcjy94” <lhs01071526524@gmail.com>
2022-06-07 17:52:43 +0900

    java commit

--- src/main/java/test05/Exam59.java
+++ src/main/java/test05/Exam59.java
@@ -43,4 +43,18 @@ public class Exam59 {
         return answer;
     }
 
+
+    public String solution3(String need, String plan) {
+        String answer = "YES";
+        Queue<Character> Q = new LinkedList<>();
+        for (char x : need.toCharArray()) Q.add(x);
+        for (char x : plan.toCharArray()) {
+            if (Q.contains(x)) {
+                if (x != Q.poll()) return "NO";
+            }
+        }
+        if (!Q.isEmpty()) return "NO";
+        return answer;
+    }
+
 }
