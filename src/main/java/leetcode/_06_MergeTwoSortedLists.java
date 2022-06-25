4f0c97218b595136acba9ed0270d5806278c19b3
“ggmcjy94” <lhs01071526524@gmail.com>
2022-06-25 15:06:01 +0900

    java commit

--- src/main/java/leetcode/_06_MergeTwoSortedLists.java
+++ src/main/java/leetcode/_06_MergeTwoSortedLists.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.List;
+
 class ListNode {
     int val;
     ListNode next;
@@ -10,18 +12,17 @@ class ListNode {
         this.val = val;
     }
     ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
+        this.val = val;
+        this.next = next;
     }
 }
 public class _06_MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode result_node = new ListNode();
-
         ListNode curr_node = result_node;
 
         while (list1 != null && list2 != null) {
-            System.out.println(list1.val);
             if (list1.val < list2.val) {
                 curr_node.next = list1;
                 list1 = list1.next;
@@ -40,5 +41,31 @@ public class _06_MergeTwoSortedLists {
             curr_node.next = list1;
         }
         return result_node.next;
+    }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode cur = head;
+
+        while (l1 != null || l2 != null) {
+            if (l1 == null) { //끝에 도달 한 경우
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else if (l1.val < l2.val){
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        return head.next;
     }
 }
