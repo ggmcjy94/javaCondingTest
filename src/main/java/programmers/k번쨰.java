dd05301a48109f2707bfd1c742f321d75ef5f334
“ggmcjy94” <lhs01071526524@gmail.com>
2022-07-05 22:06:32 +0900

    java commit

--- src/main/java/programmers/k번쨰.java
+++ src/main/java/programmers/k번쨰.java
@@ -21,9 +21,23 @@ public class k번쨰 {
 
     public static void main(String[] args) {
         k번쨰 T = new k번쨰();
-        for (int i : T.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3},{4, 4, 1},{1, 7, 3}})) {
+        for (int i : T.solution2(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3},{4, 4, 1},{1, 7, 3}})) {
             System.out.print(i + " ");
         }
+    }
+
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] answer;
+        answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = commands[i][0]-1; j < commands[i][1]; j ++) {
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2]-1);
+        }
+        return answer;
     }
 
 }
