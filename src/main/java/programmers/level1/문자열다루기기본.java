3869e500a2fcb4a37ee7aa3840de9b298af55aa7
leehyunseok <lhs01071526524@gmail.com>
2022-07-26 19:36:00 +0900

    commit
    commit

--- src/main/java/programmers/level1/문자열다루기기본.java
+++ src/main/java/programmers/level1/문자열다루기기본.java
@@ -1,2 +1,21 @@
-package programmers.level1;public class 문자열다루기기본 {
+package programmers.level1;
+
+public class 문자열다루기기본 {
+
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() != 4 && s.length() != 6) return false;
+        for (char c : s.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                System.out.println(c);
+                return false;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        문자열다루기기본 T = new 문자열다루기기본();
+        System.out.println(T.solution("1B23"));
+    }
 }
