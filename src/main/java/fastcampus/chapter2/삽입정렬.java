3a70117aa46561a1d2a6be394da7d03103f95fd1
leehyunseok <lhs01071526524@gmail.com>
2022-08-12 14:11:23 +0900

    정렬
    정렬

---
+++ src/main/java/fastcampus/chapter2/삽입정렬.java
@@ -0,0 +1,26 @@
+package fastcampus.chapter2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class 삽입정렬 {
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> dataList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            dataList.add((int) (Math.random()*100));
+        }
+
+        for (int i = 0; i < dataList.size()-1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (dataList.get(j) < dataList.get(j-1)) {
+                    Collections.swap(dataList, j, j-1);
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println(dataList);
+    }
+}
