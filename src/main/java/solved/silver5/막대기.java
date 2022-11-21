c4226a737b8bb8b197c316b6baff8ed3dd6555d7
leehyunseok <lhs01071526524@gmail.com>
2022-11-21 20:33:11 +0900

    commit
    commit

---
+++ src/main/java/solved/silver5/막대기.java
@@ -0,0 +1,24 @@
+package solved.silver5;
+
+import java.util.Scanner;
+
+public class 막대기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+
+        int count = 0;
+        int stick = 64;
+
+        while (X>0) {
+            if (stick > X) {
+               stick/=2;
+            } else {
+                count++;
+                X -= stick;
+            }
+        }
+        System.out.println(count);
+    }
+}
